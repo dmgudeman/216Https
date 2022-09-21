461120068c0082d3c72265d63061f18f2e2e02a3
davidmgudeman <davidmgudeman@gmail.com>
2022-09-21 14:26:36 -0700

    sent order to backend

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -24,6 +24,18 @@ const Cart = (props) => {
     setIsCheckout(true);
   }
 
+  const submitOrderHandler = (userData) => {
+    fetch( "https://http-cc79c-default-rtdb.firebaseio.com/orders.json",{
+    method: 'POST',
+    body: JSON.stringify({
+        user: userData,
+        orderedItems: cartCtx.items
+    }
+    )})
+
+
+  }
+
   const cartItems = (
     <ul className={classes['cart-items']}>
       {cartCtx.items.map((item) => (
@@ -56,7 +68,7 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-     {isCheckout && <Checkout onCancel={props.onClose}/>}
+     {isCheckout && <Checkout onConfirm ={submitOrderHandler} onCancel={props.onClose}/>}
       {!isCheckout && modalActions}
     </Modal>
   );
