ca987da747097fcbf084a06def48c30cb3a9d835
davidmgudeman <davidmgudeman@gmail.com>
2022-09-20 18:30:06 -0700

    loaded meals

--- src/App.js
+++ src/App.js
@@ -1,77 +1,12 @@
-import { useState, useCallback, useEffect } from 'react';
+import { useState } from 'react';
+import LoadMeals from '../src/helpers';
 
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
 import CartProvider from './store/CartProvider';
 
-
-
 function App() {
-    const [meals, setMeals] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
-  
-    const fetchMealsHandler = useCallback(async () => {
-      setIsLoading(true);
-      setError(null);
-      try {
-        const response = await fetch('https://movies-9c5b1-default-rtdb.firebaseio.com/meals.json');
-        if (!response.ok) {
-          throw new Error('Something went wrong!');
-        }
-  
-        const data = await response.json();
-        const loadedMeals = [];
-        for(const key in data) {
-          loadedMeals.push({
-              id: key,
-              name: data[key].title,
-              description: data[key].desciptionText,
-              price: data[key].price
-  
-  
-          })
-        }
-  
-       
-        setMeals(loadedMeals);
-      } catch (error) {
-        setError(error.message);
-      }
-      setIsLoading(false);
-    }, []);
-  
-    useEffect(() => {
-      fetchMealsHandler();
-    }, [fetchMealsHandler]);
-  
-    async function addMealHandler(meal) {
-      const response = await fetch('https://movies-9c5b1-default-rtdb.firebaseio.com/meals.json',{
-      method: 'POST',
-      body: JSON.stringify(meal),
-      headers: {
-          'Content-Type': 'application/json'
-      }});
-      const data = await response.json();
-      console.log(data);
-    }
-  
-    let content = <p>Found no movies.</p>;
-  
-    if (meals.length > 0) {
-      content = <MealsList meals={meals} />;
-    }
-  
-    if (error) {
-      content = <p>{error}</p>;
-    }
-  
-    if (isLoading) {
-      content = <p>Loading...</p>;
-    }
-
-
 
   const [cartIsShown, setCartIsShown] = useState(false);
 
---
+++ src/helpers.js
@@ -0,0 +1,50 @@
+const DUMMY_MEALS = [
+    {
+      id: 'm1',
+      name: 'Sushi',
+      description: 'Finest fish and veggies',
+      price: 22.99,
+    },
+    {
+      id: 'm2',
+      name: 'Schnitzel',
+      description: 'A german specialty!',
+      price: 16.5,
+    },
+    {
+      id: 'm3',
+      name: 'Barbecue Burger',
+      description: 'American, raw, meaty',
+      price: 12.99,
+    },
+    {
+      id: 'm4',
+      name: 'Green Bowl',
+      description: 'Healthy...and green...',
+      price: 18.99,
+    },
+  ];
+
+ const  LoadMeals = () => DUMMY_MEALS.map(async (meal)=>{
+
+   let  FBmeal =  {
+        
+            id: meal.id,
+            name: meal.name,
+            description: meal.description,
+            releaseDate: meal.releaseDate
+
+
+        }
+    
+    const response = await fetch('https://http-cc79c-default-rtdb.firebaseio.com/meals.json',{
+    method: 'POST',
+    body: JSON.stringify(FBmeal),
+    headers: {
+        'Content-Type': 'application/json'
+    }});
+    const data = await response.json();
+    console.log(FBmeal.name, data);
+} )
+
+export default LoadMeals;
